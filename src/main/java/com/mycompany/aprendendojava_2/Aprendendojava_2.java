9e482ae5f6e351a94867a5d5927e71bcb1778de2
evdevjunior <evdevjunior@hotmail.com>
2024-05-16 21:07:13 -0300

    primeiro commit de configuração

---
+++ src/main/java/com/mycompany/aprendendojava_2/Aprendendojava_2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aprendendojava_2;
+
+/**
+ *
+ * @author Evandro
+ */
+public class Aprendendojava_2 {
+
+    public static void main(String[] args) {
+        
+    }
+}
